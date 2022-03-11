bd20a57659f09a6894cfedb576454f6acabb77ee
Sarah <sarahnmurphy93@gmail.com>
2022-03-11 22:24:50 +0000

    Add sidebar navigation

---
+++ components/sidebar.tsx
@@ -0,0 +1,81 @@
+import {
+  Box,
+  Flex,
+  Link,
+  ChakraProps,
+  OmitCommonProps
+} from '@chakra-ui/react';
+import { DetailedHTMLProps, HTMLAttributes } from 'react';
+
+type Sidebar = JSX.IntrinsicAttributes &
+  OmitCommonProps<
+    DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>,
+    keyof ChakraProps
+  > &
+  ChakraProps & { as?: 'div' | undefined };
+
+type NavItem = { [x: string]: any; children?: any };
+
+export const Sidebar = (props: Sidebar) => (
+  <Box
+    as="nav"
+    pos="fixed"
+    top={0}
+    left={0}
+    zIndex="sticky"
+    h="full"
+    pb={10}
+    overflowX="hidden"
+    overflowY="auto"
+    borderRightWidth="1px"
+    width={60}
+    {...props}
+  >
+    <Flex minHeight="100vh" align="center" justify="center" width="100%">
+      <Flex
+        width="100%"
+        px={4}
+        py={5}
+        direction="column"
+        as="nav"
+        fontSize="md"
+        color="navy"
+        aria-label="Main Navigation"
+      >
+        <NavItem>About Me</NavItem>
+        <NavItem>Experience</NavItem>
+        <NavItem>Projects</NavItem>
+        <NavItem>Contact</NavItem>
+      </Flex>
+    </Flex>
+  </Box>
+);
+
+const NavItem = (props: NavItem) => {
+  const { children, ...rest } = props;
+
+  return (
+    <Flex width="100%" align="center" justify="center">
+      <Link width="100%" _hover={{ textDecoration: 'none' }}>
+        <Flex
+          py={3}
+          width="100%"
+          align="center"
+          justify="center"
+          _hover={{
+            bg: 'freshLemon',
+            color: 'navy',
+            borderRadius: '4px'
+          }}
+          role="group"
+          fontWeight="semibold"
+          fontSize="lg"
+          transition=".1s ease"
+          {...rest}
+        >
+          {children}
+        </Flex>
+      </Link>
+    </Flex>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,34 @@
 import type { NextPage } from 'next';
-import { Box, Heading, Text } from '@chakra-ui/react';
+import { Box, Flex, Heading } from '@chakra-ui/react';
+
+import { Sidebar } from '../components/sidebar';
 
 const Home: NextPage = () => {
   return (
     <Box
-      w="100%"
-      h="100vh"
+      as="section"
       bg="lightLime"
       p={4}
       color="navy"
       bgGradient="linear(to-br, lightLime, teal)"
+      minHeight="100vh"
     >
-      <Text>Hello there</Text>
-      <Heading color="white" size="4xl">
-        Welcome
-      </Heading>
+      <Sidebar />
+      <Flex
+        ml={{ base: 0, md: 60 }}
+        p={4}
+        pb={16}
+        minHeight="inherit"
+        direction="column"
+        justify="center"
+      >
+        <Heading color="white" size="4xl">
+          Hi,
+        </Heading>
+        <Heading color="white" size="4xl">
+          I&#39;m Sarah
+        </Heading>
+      </Flex>
     </Box>
   );
 };
