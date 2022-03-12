2f1adeb126f7a6e048969f7bff7860daaf59e105
Sarah <sarahnmurphy93@gmail.com>
2022-03-12 11:43:20 +0000

    Add contact me button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
+import * as React from 'react';
 import type { NextPage } from 'next';
-import { Box, Flex, Heading } from '@chakra-ui/react';
+import { Box, Flex, Button, Stack } from '@chakra-ui/react';
 import { motion } from 'framer-motion';
 
 import { Sidebar, MotionLetter } from '~/components';
@@ -20,42 +21,51 @@ const Home: NextPage = () => {
       <Flex
         ml={{ base: 0, md: 60 }}
         p={4}
-        pb={16}
         minHeight="inherit"
         direction="column"
         justify="center"
       >
-        <MotionBox
-          drag
-          dragConstraints={{
-            top: -50,
-            left: -20,
-            right: 80,
-            bottom: 50
-          }}
-          dragSnapToOrigin
-          cursor="pointer"
-        >
-          <Flex>
-            <MotionLetter letter="H" />
-            <MotionLetter letter="i" />
-            <MotionLetter letter="," />
-          </Flex>
-          <Flex gap={4}>
+        <Stack align="start" gap={3}>
+          <MotionBox
+            drag
+            dragConstraints={{
+              top: -50,
+              left: -20,
+              right: 80,
+              bottom: 50
+            }}
+            dragSnapToOrigin
+            cursor="pointer"
+          >
             <Flex>
-              <MotionLetter letter="I" />
-              <MotionLetter letter="'" />
-              <MotionLetter letter="m" />
+              <MotionLetter letter="H" />
+              <MotionLetter letter="i" />
+              <MotionLetter letter="," />
             </Flex>
-            <Flex>
-              <MotionLetter letter="S" />
-              <MotionLetter letter="a" />
-              <MotionLetter letter="r" />
-              <MotionLetter letter="a" />
-              <MotionLetter letter="h" />
+            <Flex gap={4}>
+              <Flex>
+                <MotionLetter letter="I" />
+                <MotionLetter letter="'" />
+                <MotionLetter letter="m" />
+              </Flex>
+              <Flex>
+                <MotionLetter letter="S" />
+                <MotionLetter letter="a" />
+                <MotionLetter letter="r" />
+                <MotionLetter letter="a" />
+                <MotionLetter letter="h" />
+              </Flex>
             </Flex>
-          </Flex>
-        </MotionBox>
+          </MotionBox>
+          <Button
+            color="white"
+            size="md"
+            variant="outline"
+            _hover={{ bg: 'freshLemon', color: 'navy' }}
+          >
+            Contact me
+          </Button>
+        </Stack>
       </Flex>
     </Box>
   );
