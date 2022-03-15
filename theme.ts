e4c3dd74d6f87aa8851672f739d52bd6670af910
Sarah <sarahnmurphy93@gmail.com>
2022-03-15 23:35:55 +0000

    Change teal colour to brandTeal

--- theme.ts
+++ theme.ts
@@ -3,7 +3,7 @@ import { extendTheme } from '@chakra-ui/react';
 type BrandColors = {
   lightLime: string;
   freshLemon: string;
-  teal: string;
+  brandTeal: string;
   mint: string;
   navy: string;
 };
@@ -11,7 +11,7 @@ type BrandColors = {
 export const colors: BrandColors = {
   lightLime: '#abd699',
   freshLemon: '#ffe26a',
-  teal: '#75c9b7',
+  brandTeal: '#75c9b7',
   mint: '#c7ddcc',
   navy: '#16123f'
 };
