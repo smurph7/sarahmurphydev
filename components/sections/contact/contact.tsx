12c81bc46092641cc70b2390190ce4855da7bb9b
Sarah <sarahnmurphy93@gmail.com>
2022-06-21 03:56:52 +0100

    Use emailjs to send email from contact page

--- components/sections/contact/contact.tsx
+++ components/sections/contact/contact.tsx
@@ -1,6 +1,35 @@
 import * as React from 'react';
-import { Box, Flex, Heading, Text } from '@chakra-ui/react';
+import { Box, Button } from '@chakra-ui/react';
+import emailjs from '@emailjs/browser';
 
 export const Contact = () => {
-  return <Box>Contact Me</Box>;
+  const sendEmail = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+
+    emailjs
+      .send(
+        process.env.NEXT_PUBLIC_CONTACT_SERVICE_ID ?? '',
+        process.env.NEXT_PUBLIC_CONTACT_DEV_TEMPLATE ?? '',
+        {
+          name: 'Sarah',
+          message: 'hi there, gib job now',
+          email: 'emaillll3@gmail.com'
+        },
+        process.env.NEXT_PUBLIC_CONTACT_PUBLIC_KEY
+      )
+      .then(
+        result => {
+          console.log(result.text);
+        },
+        error => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <Box>
+      <Button onClick={sendEmail}>Contact Me</Button>
+    </Box>
+  );
 };
