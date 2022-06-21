376d635120d97dbbc318f27c86c1967f551d0b07
Sarah <sarahnmurphy93@gmail.com>
2022-06-21 04:40:28 +0100

    Add form fields to contact page

--- components/sections/contact/contact.tsx
+++ components/sections/contact/contact.tsx
@@ -1,19 +1,45 @@
 import * as React from 'react';
-import { Box, Button } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+  Stack,
+  Textarea
+} from '@chakra-ui/react';
+import { useForm, SubmitHandler } from 'react-hook-form';
 import emailjs from '@emailjs/browser';
 
-export const Contact = () => {
-  const sendEmail = (e: { preventDefault: () => void }) => {
-    e.preventDefault();
+type FormValues = {
+  name: string;
+  message: string;
+  email: string;
+};
 
+export const Contact = () => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting }
+  } = useForm<FormValues>();
+
+  // TODO acknowledge success
+  const onSubmit: SubmitHandler<FormValues> = (data: {
+    name: string;
+    message: string;
+    email: string;
+  }) => {
     emailjs
       .send(
         process.env.NEXT_PUBLIC_CONTACT_SERVICE_ID ?? '',
         process.env.NEXT_PUBLIC_CONTACT_DEV_TEMPLATE ?? '',
         {
-          name: 'Sarah',
-          message: 'hi there, gib job now',
-          email: 'emaillll3@gmail.com'
+          name: data.name,
+          message: data.message,
+          email: data.email
         },
         process.env.NEXT_PUBLIC_CONTACT_PUBLIC_KEY
       )
@@ -28,8 +54,67 @@ export const Contact = () => {
   };
 
   return (
-    <Box>
-      <Button onClick={sendEmail}>Contact Me</Button>
-    </Box>
+    <Stack as="form" gap={5} onSubmit={handleSubmit(onSubmit)}>
+      <FormControl isInvalid={!!errors.name}>
+        <Stack gap={[3, 3, 5]}>
+          <Flex
+            direction={['column', 'column', 'row']}
+            align="center"
+            justify="center"
+            width="100%"
+            gap={[3, 3, 5]}
+          >
+            <Flex direction="column" width="100%">
+              <FormLabel htmlFor="name">Name</FormLabel>
+              <Input
+                id="name"
+                placeholder="Name"
+                {...register('name', {
+                  required: 'This is required',
+                  minLength: { value: 4, message: 'Minimum length should be 4' }
+                })}
+              />
+              <FormErrorMessage>
+                {errors.name && errors.name.message}
+              </FormErrorMessage>
+            </Flex>
+            <Flex align="center" width="100%">
+              <Flex direction="column" width="100%">
+                <FormLabel htmlFor="email">Email</FormLabel>
+                <Input
+                  id="email"
+                  placeholder="Email"
+                  {...register('email', {
+                    required: 'This is required'
+                    // TODO Validate email
+                  })}
+                />
+                <FormErrorMessage>
+                  {errors.email && errors.email.message}
+                </FormErrorMessage>
+              </Flex>
+            </Flex>
+          </Flex>
+          <Flex align="center" width="100%">
+            <Flex direction="column" width="100%">
+              <FormLabel htmlFor="Message">Message</FormLabel>
+              <Textarea
+                id="message"
+                placeholder="Message"
+                {...register('message', {
+                  required: 'This is required'
+                })}
+              />
+              <FormErrorMessage>
+                {errors.message && errors.message.message}
+              </FormErrorMessage>
+            </Flex>
+          </Flex>
+        </Stack>
+      </FormControl>
+      <Flex>
+        <Button type="submit">Contact Me</Button>
+      </Flex>
+    </Stack>
   );
 };
